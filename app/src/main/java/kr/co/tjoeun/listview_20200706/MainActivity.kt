0606572718c4baa5a3998c49f04195334606a3e1
김재영 <kjymonster@naver.com>
2020-07-06 21:46:36 +0900

    어댑터 + 목록 조합 =>리스트뷰에 연결

--- app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
@@ -2,12 +2,16 @@ package kr.co.tjoeun.listview_20200706
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.listview_20200706.adapters.StudentAdapter
 import kr.co.tjoeun.listview_20200706.datas.Student
 
 class MainActivity : AppCompatActivity() {
 
     //학생 목록을 담고 있는 변수
     val mStudentList = ArrayList<Student>()
+
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,11 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("조장우", "서울시 종로구", 1983))
         mStudentList.add(Student("채정실", "서울시 용산구", 1991))
 
+        //미뤄둔 어댑터 객체화를 실행
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        //완성된 어댑터를 리스트뷰와 연결
+        studentListView.adapter = mAdapter
 
 
 
