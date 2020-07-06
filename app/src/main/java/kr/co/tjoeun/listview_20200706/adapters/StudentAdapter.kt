e5a1769ff1aef3e9a76713699d926d10a8351257
김재영 <kjymonster@naver.com>
2020-07-06 21:58:17 +0900

    실제 학생 데이터를 출력  *나이가 생년이라, 나이로 바꿔야함

--- app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.listview_20200706.R
 import kr.co.tjoeun.listview_20200706.datas.Student
 
 class StudentAdapter(val mContext : Context, val resId:Int, val mList: List<Student>) : ArrayAdapter<Student>(mContext, resId, mList){
-    
+    //만들 장소 (Main에서 this)
+
     //화면을 그려줄때 사용하는 객체
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        //position: == Array의 위치
         var tempRow = convertView //임시 Row로 null인지 확인, null이라면 inf로 채워준다.
         //받아본 임시 row가 null인가?
         if(tempRow == null){
@@ -25,7 +28,24 @@ class StudentAdapter(val mContext : Context, val resId:Int, val mList: List<Stud
         
           //tempRow에는 null일 가능성이 없다.
         //진짜 row에는 절대 null이 아니게 된 (!! = null이 절대 아님) tempRow를 복사.
-        val row = tempRow!! 
+        val row = tempRow!!
+
+
+        // **마무리 단계**
+        //row의 내용을 실제에 맞게 변경
+        //(그려줄 위치에 맞는(position) 실제 데이터를 가져오고
+        val data = mList[position]
+
+        //row의 컴포넌트들을 가져와서
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+        //데이터 반영
+        nameTxt.text = data.name
+        addressTxt.text = data.address
+
+        ageTxt.text = "${data.birthYear}세"
         
          //ListView에 이 row를 뿌려달라고 결과로 지정.
         return row
